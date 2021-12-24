b2a59cdd7a377ca381f2e012fe194b917a2408e4
liuzihao0000 <liu.0513@qq.com>
2021-12-24 17:36:17 +0800

    新增文件

---
+++ src/main/java/com/yida/gitdemo/controller/IndexController.java
@@ -0,0 +1,12 @@
+package com.yida.gitdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//返回字符串
+public class IndexController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello world";//返回字符串
+    }
+}
